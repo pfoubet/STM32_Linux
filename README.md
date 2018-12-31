efc80b417af42dfbaa88f8d5c3d4bb947213ebe7
debian <debian@localhost.localdomain>
2018-12-31 15:22:23 +0100

    Version 2.7

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # STM32_Linux
 Install pour Debian Stretch
+
+Cours de P. Foubet
+
---
+++ recolle.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+cat STM32Laa STM32Lab STM32Lac STM32Lad STM32Lae STM32Laf STM32Lag STM32Lah STM32Lai > install_sw4stm32_linux_64bits-v2.7.run
+
